d1f916be1398f59906475004a6955496e207a71a
tehsunnliu <tehsunnliu@gmail.com>
2021-11-17 09:57:37 +0800

    change colors

--- packages/react-components/src/ChainImg.tsx
+++ packages/react-components/src/ChainImg.tsx
@@ -34,6 +34,7 @@ function ChainImg ({ className = '', isInline, logo, onClick, withoutHl }: Props
       alt='chain logo'
       className={`${className}${(isEmpty && !withoutHl) ? ' highlight--bg' : ''}${isInline ? ' isInline' : ''}`}
       onClick={onClick}
+      style={{background: "transparent", borderRadius: 0}}
       src={require("./../../../assets/images/logo.png")}
     />
   );

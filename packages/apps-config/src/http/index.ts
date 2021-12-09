45becaa99abde57d5747e7f04214e07d9638fa71
tehsunnliu <tehsunnliu@gmail.com>
2021-12-09 16:52:57 +0800

    fix config

--- packages/apps-config/src/http/index.ts
+++ packages/apps-config/src/http/index.ts
@@ -2,7 +2,7 @@ let httpUrl:string;
 let linkUrl: string;
 if(process.env.NODE_ENV === "production"){
   httpUrl = 'http://106.15.44.155:4399';
-  linkUrl = 'http://data.cesslab.co.uk/'
+  linkUrl = 'http://data.cesslab.co.uk/data'
   // linkUrl = 'http://121.46.19.38:54558'
 } else {
   httpUrl = 'http://106.15.44.155:4399';
